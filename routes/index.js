158808d61dfe53441d2c4cf6175218001ddeaf74
KobinaOtc <kobinaotchereadjoku@gmail.com>
2026-03-13 02:09:25 +0000

    update

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 
+router.use('/', require('./swagger')); // Add this line to include the swagger route
+
 router.get('/', (req, res) => {
+    //#swagger.tags = ['Hello World']
     res.send('Hello World!');
 });
 
